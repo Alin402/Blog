c5e1afaf2b5cc9f74f29e55cd6ff8bae24a84228
Alin402 <balanescu.alin03@gmail.com>
2020-02-15 15:45:06 +0200

    Updates

---
+++ precache-manifest.96e8628577ce1e3d314522fdd8e9a409.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b29b07a7eca41d02365123f7cc5d366",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "d78bc13713dc9f8216a2",
+    "url": "/Blog/static/css/main.643c85e0.chunk.css"
+  },
+  {
+    "revision": "df79025ef5efb4a40432",
+    "url": "/Blog/static/js/2.2a00e99c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Blog/static/js/2.2a00e99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78bc13713dc9f8216a2",
+    "url": "/Blog/static/js/main.699f1aca.chunk.js"
+  },
+  {
+    "revision": "954117ddda4d9b1aeb9b",
+    "url": "/Blog/static/js/runtime-main.289e438d.js"
+  },
+  {
+    "revision": "c2c4b590b2093c2ee3f76e296dee24d2",
+    "url": "/Blog/static/media/back.c2c4b590.jpg"
+  },
+  {
+    "revision": "704e78d80fa08beba24ce4bcfd077f22",
+    "url": "/Blog/static/media/img.704e78d8.jpg"
+  }
+]);
