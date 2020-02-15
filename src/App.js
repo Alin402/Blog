4c5e61a280b1efbad8d6edbca49effdcc4bf6787
Alin402 <balanescu.alin03@gmail.com>
2020-02-15 16:05:07 +0200

    removed particlesjs

--- src/App.js
+++ src/App.js
@@ -12,31 +12,6 @@ class App extends React.Component {
     return(
       <div>
         <Layout>
-        <Particles 
-              params={{
-            		particles: {
-                  number: {
-                    value: '50'
-                  },
-            			line_linked: {
-            				shadow: {
-            					enable: true,
-            					color: "#3CA9D1",
-            					blur: 5
-            				}
-            			}
-            		}
-            	}}
-              style={{
-                width: '100%',
-                position: 'fixed',
-                top: '0',
-                bottom: '0',
-                right: '0',
-                left: '0',
-                zIndex: '-1'
-              }}
-            />
             <Route exact path='/about' component ={About}/>
             <Route exact path='/contact' component ={Contact}/>
             <Route exact path='/portofolio' component ={Portofolio}/>
